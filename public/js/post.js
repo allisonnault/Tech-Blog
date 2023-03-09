9e568f4a313d4f4588a88223c46b752e13bd704d
allisonnault <allisonrnault@gmail.com>
2023-03-08 19:15:56 -0600

    saving for help '

--- public/js/post.js
+++ public/js/post.js
@@ -13,7 +13,7 @@ const newPostHandler = async (event) => {
         });
 
         if (response.ok) {
-            document.location.replace('/');
+            document.location.reload();
         } else {
             alert('Failed to add post');
         }

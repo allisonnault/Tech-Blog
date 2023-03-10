f49124c523ebe731b42c91a3369244f9055fb28a
allisonnault <allisonrnault@gmail.com>
2023-03-09 18:22:18 -0600

    removed test code from schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,3 @@ CREATE DATABASE tech_blog_db;
 
 
 
-
-SELECT * , users.username AS user FROM post JOIN users ON post.user_id = users.id
-
-SELECT username FROM users WHERE post.user_id = users.id AS user
